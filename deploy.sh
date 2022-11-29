bcdaebb7ea75abe9abdacd1d9d5bc8f80b5ed86a
coderQls <1064341174@qq.com>
2022-11-29 14:59:21 +0800

    修复deploy

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+npm run build
+
 # 进入生成的文件夹
-cd ./public
+cd ./vuepress/dist
 
 git init
 git add -A
-git commit -m '--first commit'
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/coderQls/my-blog.git main
+git push -f https://github.com/coderQls/my-blog.git deploy
 
 cd -
