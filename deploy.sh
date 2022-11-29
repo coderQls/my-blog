11e88d2352c9553cca0c5b7c375327e543681de0
coderQls <1064341174@qq.com>
2022-11-29 15:44:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/coderQls/my-blog.git deploy
+git push -f git@github.com:coderQls/my-blog.git deploy
 
 cd -
