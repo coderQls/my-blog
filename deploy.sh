8c07ef0855c4032f37a86e33c3e5e471b80d7942
coderQls <1064341174@qq.com>
2022-11-29 16:31:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 # deploy.sh文件
+###
+ # @Author: coderQls 1064341174@qq.com
+ # @Date: 2022-11-29 16:30:24
+ # @LastEditors: coderQls 1064341174@qq.com
+ # @LastEditTime: 2022-11-29 16:30:45
+ # @Description: 
+### 
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -14,4 +21,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:coderQls/my-blog.git deploy
 
-cd -
+cd ..
