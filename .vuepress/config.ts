40c8744cc313b7d38c9d0188cc55f012c3491877
coderQls <1064341174@qq.com>
2022-11-29 17:26:37 +0800

    deploy

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,7 +3,7 @@ import type { DefaultThemeOptions } from 'vuepress'
 import recoTheme from 'vuepress-theme-reco'
 
 export default defineUserConfig({
-  base: '/my_blog/',
+  base: '/my-blog/',
   title: 'coderQls的博客',
   description: '学习记录',
   theme: recoTheme({
