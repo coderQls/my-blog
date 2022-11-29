8a577118e14a7dd98bbcbd59616077bf4a5e263c
coderQls <1064341174@qq.com>
2022-11-29 16:38:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,13 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd ./vuepress/dist
+# cd ./vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:coderQls/my-blog.git deploy
+git push -f git@github.com:coderQls/my-blog.git main
 
 cd ..
